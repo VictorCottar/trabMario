677be7a7d3dc3f6cd9451695d14b80552de5c335
VictorCottar <victorcottarms@outlook.com>
2024-09-12 20:59:30 -0300

    Subindo o projeto

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'archivo': ['Archivo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
